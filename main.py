fe97b739e8ad20e2e69a3bd0b4460dcc0356360b
zhu <zhu>
2024-06-08 12:10:40 +0800

    fix:修正檔案計數為時間

--- main.py
+++ main.py
@@ -2,35 +2,53 @@ import os
 import requests
 import gzip
 import shutil
+import time
 
 download_dir = 'files'
 if not os.path.exists(download_dir):
     os.makedirs(download_dir)
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept': '*/*',
+    'Connection': 'keep-alive'
+}
 
 # download and extract
 def download_file(url, download_dir):
     filename = url.split('/')[-1]
     filepath = os.path.join(download_dir, filename)
 
-    response = requests.get(url, stream=True)
+    try:
+        response = requests.get(url, headers=headers, stream=True)
+        response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        print(f'Fail to download {filename}: {e}')
+        return
+    
     if response.status_code == 200:
         with open(filepath, 'wb') as f:
             f.write(response.content)
         print(f'Downloaded {filename}')
 
-        with gzip.open(filepath, 'rb') as f_in:
-            with open(filepath[:-3], 'wb') as f_out:
-                shutil.copyfileobj(f_in, f_out)
-        print(f'Extracted {filename[:-3]}')
+        try:
+            with gzip.open(filepath, 'rb') as f_in:
+                with open(filepath[:-3], 'wb') as f_out:
+                    shutil.copyfileobj(f_in, f_out)
+            print(f'Extracted {filename[:-3]}')
 
-        os.remove(filepath)
+            os.remove(filepath)
+        except Exception as e:
+            print(f'Failed to extract {filename}: {e}')
     else:
-        print(f'Failed to download {filename}')
+        print(f'Failed to download {filename}: HTTP {response.status_code}')
 
-base_url = 'https://tisvcloud.freeway.gov.tw/history/motc20/Section/20240607/LiveTraffic_'
+base_url = 'https://tisvcloud.freeway.gov.tw/history/motc20/Section/20240606/LiveTraffic_'
 
-
-for i in range(10):
-    file_number = str(i).zfill(4)
-    file_url = f'{base_url}{file_number}.xml.gz'
-    download_file(file_url, download_dir)
+for hour in range(24):
+    for minute in range(60):
+        file_number = f'{hour:02}{minute:02}'
+        file_url = f'{base_url}{file_number}.xml.gz'
+        download_file(file_url, download_dir)
+        time.sleep(5)                  # delay
